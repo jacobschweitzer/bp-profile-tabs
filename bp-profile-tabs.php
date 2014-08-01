e1c154201191171f8348551f6c42028a2d579cc4
Jacob Schweitzer <allambition@gmail.com>
2014-08-01 11:19:48 -0300

    changed headers, removed un-needed file calls and did all todos

--- bp-profile-tabs.php
+++ bp-profile-tabs.php
@@ -1,18 +1,13 @@
 <?php
 
 /**
- * The WordPress Plugin Boilerplate.
  *
- * A foundation off of which to build well-documented WordPress plugins that
- * also follow WordPress Coding Standards and PHP best practices.
- *
- * @package   Plugin_Name
- * @author    Your Name <email@example.com>
+ * @package   BP_Profile_Tabs
+ * @author    Jacob Schweitzer <allambition@gmail.com>
  * @license   GPL-2.0+
- * @link      http://example.com
- * @copyright 2014 Your Name or Company Name
+ * @link      http://ijas.me
+ * @copyright 2014 Jacob Schweitzer
  *
- * @wordpress-plugin
  * Plugin Name:       BuddyPress Profile Tabs
  * Plugin URI:        http://ijas.me
  * Description:       Makes the profile groups BuddyPress provides into tabs to better organize and consolidate the profile page. 
@@ -23,32 +18,17 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * WordPress-Plugin-Boilerplate-Powered: v1.0.0
  */
+
 // If this file is called directly, abort.
 if ( !defined( 'WPINC' ) ) {
 	die;
 }
 
-/* ----------------------------------------------------------------------------*
- * Public-Facing Functionality
- * ---------------------------------------------------------------------------- */
-
-
-
 /*
- * @TODO:
- *
- * - replace `class-plugin-name.php` with the name of the plugin's class file
- *
+ * Load Main Plugin File
  */
-require_once( plugin_dir_path( __FILE__ ) . 'public/class-plugin-name.php' );
-
-/*
- * Load template system
- */
-require_once( plugin_dir_path( __FILE__ ) . 'includes/template.php' );
-
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-bp-profile-tabs.php' );
 
 /*
  * Load Language wrapper function for WPML/Ceceppa Multilingua/Polylang
@@ -59,44 +39,17 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/language.php' );
  * Register hooks that are fired when the plugin is activated or deactivated.
  * When the plugin is deleted, the uninstall.php file is loaded.
  *
- * @TODO:
- *
- * - replace Plugin_Name with the name of the class defined in
- *   `class-plugin-name.php`
  */
-register_activation_hook( __FILE__, array( 'Plugin_Name', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'Plugin_Name', 'deactivate' ) );
+register_activation_hook( __FILE__, array( 'BP_Profile_Tabs', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'BP_Profile_Tabs', 'deactivate' ) );
+
+
+add_action( 'plugins_loaded', array( 'BP_Profile_Tabs', 'get_instance' ) );
 
 /*
- * @TODO:
- *
- * - replace Plugin_Name with the name of the class defined in
- *   `class-plugin-name.php`
+ * Load Admin Settings Page
  */
-add_action( 'plugins_loaded', array( 'Plugin_Name', 'get_instance' ) );
-
-/* ----------------------------------------------------------------------------*
- * Dashboard and Administrative Functionality
- * ---------------------------------------------------------------------------- */
-
-/*
- * @TODO:
- *
- * - replace `class-plugin-name-admin.php` with the name of the plugin's admin file
- * - replace Plugin_Name_Admin with the name of the class defined in
- *   `class-plugin-name-admin.php`
- *
- * If you want to include Ajax within the dashboard, change the following
- * conditional to:
- *
- * if ( is_admin() ) {
- *   ...
- * }
- *
- * The code below is intended to to give the lightest footprint possible.
- */
-if ( is_admin() && (!defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
-
-	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-plugin-name-admin.php' );
-	add_action( 'plugins_loaded', array( 'Plugin_Name_Admin', 'get_instance' ) );
+if ( is_admin() ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-bp-profile-tabs-admin.php' );
+	add_action( 'plugins_loaded', array( 'BP_Profile_Tabs_Admin', 'get_instance' ) );
 }
